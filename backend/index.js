5cc1a7c73acaa959383302f421cd6fcc28eadd2c
andrewxxclark <68085199+andrewxxclark@users.noreply.github.com>
2020-08-30 17:01:00 -0400

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const morgan = require('morgan');
+const multer = require('multer');
+const path = require('path');
+
+//Inicializaciones
+const app = express();
+
+//Configuraciones
+app.set('port', 3000);
+
+//middlewares
+app.use(morgan('dev'));
+const storage = multer.diskStorage({
+    destination: path.join(__dirname,'public/uploads'),
+    filename(req, file, cb) {
+        cb(null, new Date().getTime() + path.extname(file.originalname));
+    }
+})
+app.use(multer({storage}).single('image'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//Rutas
+app.use('/api/books', require('./routes/books'));
+
+//Archivos estaticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Inicia el servidor
+app.listen(app.get('port'), () => {
+    console.log('servidor en el puerto', app.get('port'));
+});
