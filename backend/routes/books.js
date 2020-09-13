11cb1c6287508d977cbeddb9f2acdcf814718236
andrewxxclark <68085199+andrewxxclark@users.noreply.github.com>
2020-09-13 02:17:43 -0400

    API Developed

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,7 +1,26 @@
 const { Router } = require('express');
 const router = Router();
 
-router.get('/', (req, res) => res.json({"Text": "Hello world"}));
+const Book = require('../models/Book');
+
+router.get('/', async(req, res) => {
+    const books = await Book.find();
+    res.json(books);
+
+});
+
+router.post('/', async (req, res) => {
+
+    const { title, author, isbn}= req.body;
+    const newBook = new Book({title, author, isbn})
+    await newBook.save(); //Guarda el registro del libro en la BD
+    res.json({message: 'Book Saved'});
+});
+
+router.delete('/:id', async (req, res) => {
+    await Book.findByIdAndDelete(req.params.id); //Metodo de MongoDB para encontrar y eliminar datos a través de su ID
+    res.json({message: 'Book Deleted'});
+})
 
 module.exports = router;
 
